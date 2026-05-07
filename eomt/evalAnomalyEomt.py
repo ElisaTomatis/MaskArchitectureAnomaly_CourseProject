115f198ecdc22b77325b2b785dc8a366098c5def
frfdvd <davidefurfaro03@gmail.com>
2026-05-07 13:06:19 +0200

    modified evalAnomalyEomt

--- eomt/evalAnomalyEomt.py
+++ eomt/evalAnomalyEomt.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import os
 import cv2
+import yaml
 import glob
 import torch
 import random
@@ -29,17 +30,15 @@ NUM_CLASSES = 20
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = True
 
-input_transform = Compose(
-    [
-        Resize((512, 1024), Image.BILINEAR),
-        ToTensor(),
-        # Normalize([.485, .456, .406], [.229, .224, .225]),
-    ]
-)
+input_transform = Compose([
+    Resize((1280, 1280), Image.BILINEAR), # EoMT Giant usa solitamente 1280
+    ToTensor(),
+    Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]), # Standard ImageNet/DINO
+])
 
 target_transform = Compose(
     [
-        Resize((512, 1024), Image.NEAREST),
+        Resize((1280, 1280), Image.NEAREST),
     ]
 )
 
@@ -63,6 +62,15 @@ def main():
     parser.add_argument('--cpu', action='store_true')
     args = parser.parse_args()
     
+    model = EoMT(NUM_CLASSES)
+
+    if (not args.cpu):
+        model = torch.nn.DataParallel(model).cuda()
+
+    config_path = "configs/dinov2/coco/panoptic/eomt_giant_1280.yaml"  # TODO: change to the config file
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+
     name = config.get("trainer", {}).get("logger", {}).get("init_args", {}).get("name")
     
     if name is None:
@@ -73,35 +81,22 @@ def main():
                 repo_id=f"tue-mps/{name}",
                 filename="pytorch_model.bin",
             )
-
-            is_dinov3 = "dinov3" in name
-
-            if is_dinov3:
-                model_kwargs["ckpt_path"] = state_dict_path
-                model_kwargs["delta_weights"] = True
-
-            model = (
-                lit_cls(
-                    img_size=data.img_size,
-                    num_classes=data.num_classes,
-                    network=network,
-                    **model_kwargs,
-                )
-                .eval()
-                .to(device)
+ 
+            state_dict = torch.load(
+                state_dict_path, weights_only=True
             )
+            model.load_state_dict(state_dict, strict=False)
 
-            if not is_dinov3:
-                state_dict = torch.load(
-                    state_dict_path, map_location=f"cuda:{device}", weights_only=True
-                )
-                model.load_state_dict(state_dict, strict=False)
+            print ("Model and weights LOADED successfully")
 
         except RepositoryNotFoundError:
             warnings.warn(
                 f"Pre-trained model not found for `{name}`. Please load your own checkpoint."
             )
 
+    model.eval()
+
+    print('cao')
 
     anomaly_score_logit_list = []
     anomaly_score_softmax_list = []
@@ -110,39 +105,13 @@ def main():
 
     if not os.path.exists('results.txt'):
         open('results.txt', 'w').close()
-    file = open('results.txt', 'a')
+    file = open('results.txt', 'w')
 
-    modelpath = args.loadDir + args.loadModel
-    weightspath = args.loadDir + args.loadWeights
-
-    print ("Loading model: " + modelpath)
-    print ("Loading weights: " + weightspath)
-
-    model = ERFNet(NUM_CLASSES)
-
-    if (not args.cpu):
-        model = torch.nn.DataParallel(model).cuda()
-
-    def load_my_state_dict(model, state_dict):  #custom function to load model when not all dict elements
-        own_state = model.state_dict()
-        for name, param in state_dict.items():
-            if name not in own_state:
-                if name.startswith("module."):
-                    own_state[name.split("module.")[-1]].copy_(param)
-                else:
-                    print(name, " not loaded")
-                    continue
-            else:
-                own_state[name].copy_(param)
-        return model
-
-    model = load_my_state_dict(model, torch.load(weightspath, map_location=lambda storage, loc: storage))
-    print ("Model and weights LOADED successfully")
-    model.eval()
     
     for path in glob.glob(os.path.expanduser(str(args.input[0]))):
         print(path)
         images = input_transform((Image.open(path).convert('RGB'))).unsqueeze(0).float().cuda()
+        print(images.shape)
         # images = images.permute(0,3,1,2)
         with torch.no_grad():
             result = model(images)
